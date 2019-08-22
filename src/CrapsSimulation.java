51e27e90b2678f9cec1ca109d9dbbdedadddd82b
lauren.sciullo <lauren.sciullo@MU-C1-06.mullen.pvt.k12.co.us>
2019-08-22 11:55:36 -0600

    Almost done stage

--- src/CrapsSimulation.java
+++ src/CrapsSimulation.java
@@ -10,9 +10,7 @@ public class CrapsSimulation
 				System.out.println("Welcome to the Craps Simulation. Good luck! Enter your name to begin: ");
 				String name = userInput.nextLine();
 				
-				boolean result = true;
-				while (result) 
-					{
+				
 						
 				int roll1 = (int)(Math.random()*6+1);
 				int roll2 = (int)(Math.random()*6+1);
@@ -22,18 +20,39 @@ public class CrapsSimulation
 				if (rollResult ==7 )
 					{
 						System.out.println("You rolled a " + roll1 + " and a  " + roll2 + " for a total of 7. You win, " + name);
-						result = false;
+						
 					}
 				else if (rollResult == 12)
 					{
 						System.out.println("You rolled a " + roll1 + " and a " + roll2 + " for a total of 12. Sorry, but you lose, " + name);
-						result = false;
+						
 					}
 				else 
 					{
 						System.out.println("You rolled a " + roll1 + " and a " + roll2 + ". Your point is " + rollResult + ", " +name);
+						
+						boolean result = true;
+						while (result)
+							{
+								int try1 = (int)(Math.random()*6+1);
+								int try2 = (int)(Math.random()*6+1);
+								int tryResult = try1 + try2;
+								
+								if (tryResult != rollResult || tryResult != 7)
+									{
+								System.out.println("Your point is " + rollResult + ", but you rolled a " + try1 + " and a " + try2 + " for a total of " + tryResult);
+									}
+								else if (tryResult == 7)
+									{
+										System.out.println(name + ", sorry, you rolled a 7. You lose!");
+									}
+								else 
+									{
+										System.out.println(name + ", you win!");
+									}
+							}
 					}
 					
-			}
+			
 			}
 	}
