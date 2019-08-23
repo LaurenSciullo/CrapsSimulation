44f365c2fb65751115b3df6e36a0335281a62890
lauren.sciullo <lauren.sciullo@MU-C1-06.mullen.pvt.k12.co.us>
2019-08-23 11:55:14 -0600

    More changes

--- src/CrapsSimulation.java
+++ src/CrapsSimulation.java
@@ -17,46 +17,55 @@ public class CrapsSimulation
 				
 				int rollResult = roll1 + roll2;
 				
-				if (rollResult ==7 )
+				if (rollResult == 7 || rollResult == 11)
 					{
-						System.out.println("You rolled a " + roll1 + " and a  " + roll2 + " for a total of 7. You win, " + name);
+						System.out.println("You rolled a " + roll1 + " and a  " + roll2 + " for a total of " + rollResult + ". You win, " + name);
 						
 					}
-				else if (rollResult == 12)
+				else if (rollResult == 12 || rollResult == 2)
 					{
-						System.out.println("You rolled a " + roll1 + " and a " + roll2 + " for a total of 12. Sorry, but you lose, " + name);
+						System.out.println("You rolled a " + roll1 + " and a " + roll2 + " for a total of " + rollResult + ". Sorry, but you lose, " + name);
 						
 					}
-				else 
+				
+					else
 					{
 						System.out.println("You rolled a " + roll1 + " and a " + roll2 + ". Your point is " + rollResult + ", " +name);
 						
+					
+				
+				
+				boolean rollAgain = true;
+				while (rollAgain) 
+					{
 						
-								int try1 = (int)(Math.random()*6+1);
-								int try2 = (int)(Math.random()*6+1);
-								int tryResult = try1 + try2;
-								
-								
-								if (tryResult != rollResult && tryResult != 7)
-									{
-								System.out.println("Your point is " + rollResult + ", but you rolled a " + try1 + " and a " + try2 + " for a total of " + tryResult);
-								
-									}
-								else if (tryResult == 7)
-									{
-										System.out.println(name + ", sorry, you rolled a 7. You lose!");
-										
-									}
-								else 
-									{
-										System.out.println(name + ", you win!");
-									
-									
-									}
-							
+						
+						
+					int try1 = (int)(Math.random()*6+1);
+					int try2 = (int)(Math.random()*6+1);
+					int tryResult = try1 + try2;
+						
+					
+					if (tryResult != rollResult && tryResult != 7)
+						{
+							System.out.println("Press enter to roll again");
+							String fake = userInput.nextLine();
+					System.out.println("\nYour point is " + rollResult + ", but you rolled a " + try1 + " and a " + try2 + " for a total of " + tryResult);
+					
+					rollAgain = false;
+						}
+					else if (tryResult == 7)
+						{
+							System.out.println("\n" + name + ", sorry, you rolled a " + try1 + " and a " + try2 + " for a total of 7. You lose!");
+							rollAgain = false;			
+						}
+					else 
+						{
+							System.out.println("\n" + name + ", you win! You rolled a " + try1 + " and a " + try2);
+							rollAgain = true;
 					}
 								
-					
-			
+					}
+					}
 			}
 	}
